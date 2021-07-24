c8d17e37afbbfe84c316929e199cbfa2f15b57f3
Pham Tan Lam <lamlampnqn12@gmail.com>
2021-07-24 13:32:33 +0700

    Set up Client + Server

---
+++ client/src/App.js
@@ -0,0 +1,23 @@
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+
+import PageRender from './PageRender';
+
+import Login from './pages/login';
+import Register from './pages/register';
+
+function App() {
+	return (
+		<Router>
+			<input type='checkbox' id='theme' />
+
+			<div className='App'>
+				<div className='main'>
+					<Route exact path='/:page' component={PageRender} />
+					<Route exact path='/:page/:id' component={PageRender} />
+				</div>
+			</div>
+		</Router>
+	);
+}
+
+export default App;
